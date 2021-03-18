e8b3617bb503ae23c8c6a6ecbe0988232466e817
bens1320 <yu1piao09@163.com>
2021-03-19 04:02:37 +0800

    flish cell module

---
+++ src/components/Cell.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="cell" @click="a = true">
+    <template v-if="a">x</template>
+    <template v-else>空</template>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function data() {
+    return {
+      a: false
+    }
+  }
+}
+</script>
+
+<style scoped>
+.cell{
+  border: 1px solid red;
+  width: 100px;
+  height: 100px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 60px;
+
+}
+</style>
